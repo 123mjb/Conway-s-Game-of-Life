7054f2a2ae63bd55078d45d54d1d4e20587c6cef
Micah Butt <micahbutt@outlook.com>
2025-07-03 23:08:55 +0100

    typescript

---
+++ scripts/maingame.ts
@@ -0,0 +1,173 @@
+
+var grid;
+
+// ctx.fillStyle = "rgb(200 0 0)";
+// ctx.fillRect(10, 10, 50, 50);
+// ctx.fillStyle = "rgb(0 0 200 / 50%)";
+// ctx.fillRect(30, 30, 50, 50);
+let canvas: HTMLElement|null = document.getElementById('Canvas');
+if (canvas !== null)window.addEventListener('load', function () { grid = new ConwayGrid(canvas.clientWidth, canvas.clientHeight); grid.DrawGrid() }, false)
+window.addEventListener('keydown', (e) => { grid.KeyPresses(e) })
+
+var slider = document.getElementById("Slider");
+var output = document.getElementById("SpeedOut");
+var button = document.getElementById("butn");
+
+if (button!==null)button.addEventListener('click',(e)=>grid.ButtonClicked(e))
+if (slider!==null)slider.addEventListener('change',(e)=>grid.UpdateSpeed(e))
+
+
+class ConwayGrid {
+    interval: number;
+    IntervalID: number| undefined;
+    running: boolean;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    arra: any[];
+    canvas: HTMLElement | null;
+    ctx: any;
+    slider: HTMLElement | null;
+    output: HTMLElement | null;
+    constructor(Width:number, Height:number) {
+        this.interval = 2000;
+        this.running = false;
+        this.width = Width;
+        this.height = Height;
+        this.x = Math.trunc(this.width / 5);
+        this.y = Math.trunc(this.height / 5);
+        this.arra = this.makeArray(this.x,this.y);
+        this.canvas = document.getElementById('Canvas');
+        this.ctx = null;
+        if (this.canvas !== null && 'getContext' in this.canvas) {
+            this.ctx = (this.canvas as HTMLCanvasElement).getContext('2d');
+        }
+        this.slider = document.getElementById("Slider");
+        this.output = document.getElementById("SpeedOut");
+        var button = document.getElementById("butn");
+        if (this.canvas !== null) {
+            this.canvas.addEventListener('click', (event) => { this.ClickGrid(event); this.DrawGrid() });
+        }
+        
+        this.UpdateSpeed()
+    }
+    ClickGrid(event: MouseEvent) {
+        if (!this.canvas) return;
+        const rect = this.canvas.getBoundingClientRect();
+        const x = Math.trunc((event.pageX - rect.left) / 5),
+            y = Math.trunc((event.pageY - rect.top - window.scrollY) / 5);
+        if (this.arra[x - 1][y - 1] === 1) {
+            this.arra[x - 1][y - 1] = 0;
+        }
+        else {
+            this.arra[x - 1][y - 1] = 1;
+        }
+
+    }
+    DrawGrid() {
+        for (let i = 0; i < this.x; i++) {
+            for (let j = 0; j < this.y; j++) {
+                if (this.arra[i][j] === 1) {
+                    this.ctx.fillStyle = "rgb(0 0 0)";
+                }
+                else {
+                    this.ctx.fillStyle = "rgb(255 255 255)";
+                }
+                this.ctx.fillRect(5 * i, 5 * j, 5, 5);
+            }
+        }
+    }
+    KeyPresses(e: KeyboardEvent) {
+        if (e.code === "Space") {
+            this.ChangeRunning()
+        }
+    }
+    Run() {
+        this.IntervalID = setInterval(() => {
+            let next = this.makeArray(this.x, this.y);
+            for (let i = 0; i < this.x; i++) {
+                for (let j = 0; j < this.y; j++) {
+                    var total = this.FindTotalSurrounding(i, j);
+                    if (total < 2) {
+                        next[i][j] = 0; // Underpopulation
+                    } else if (total > 3) {
+                        next[i][j] = 0; // overpopulation
+                    } else if (total === 3) {
+                        next[i][j] = 1;
+                    }else if (total === 2 && this.arra[i][j] === 1) {
+                        next[i][j] = 1; // stays alive
+                    }else {
+                        next[i][j] = 0;
+                    }
+                }
+            }
+            this.arra = next;
+            //console.log(this.arra);
+            this.DrawGrid();
+        }, this.interval);
+        // The above code runs every 2000 milliseconds, you can adjust this value for speed
+    }
+    Stop() {
+        clearInterval(this.IntervalID);
+    }
+    UpdateSpeed(){
+        if (this.slider === null || this.output === null) return;
+        console.log(this.slider.value);
+        let val =10*(this.slider.value-1);
+        this.interval=val;
+        this.output.innerHTML = ((val)+"ms")
+        if (this.running){
+            this.Stop();
+            this.Run();
+        }
+    }
+    ButtonClicked(e){
+        this.ChangeRunning()
+    }
+    ChangeRunning(){
+        this.running=!this.running
+        if (button===null)return;
+        if (this.running){
+            this.Run()
+            button.innerText="Stop"
+        }else{
+            this.Stop()
+            button.innerText="Start"
+        }
+    }
+
+
+    FindTotalSurrounding(i, j) {
+        let total = 0;
+        [-1, 0, 1].forEach(dx => {
+            [-1, 0, 1].forEach(dy => {
+                total += this.GetValue(i + dx, j + dy);
+            });
+        });
+        if (this.arra[i][j] === 1) {
+            total--; // Don't count the current cell
+        }
+        return total;
+    }
+    GetValue(x, y) {
+        if (x < 0 || x >= this.x || y < 0 || y >= this.y) {
+            return 0; // Out of bounds
+        }
+        if ((this.arra[x][y] !== 0 && this.arra[x][y] !== 1)) {
+            return 0; //not defined
+        }
+        return this.arra[x][y];
+    }
+    makeArray(d1, d2) {
+    let finalarr = new Array(d1)
+    for (let i = 0; i < d1; i++) {
+        finalarr[i] = new Array(d2).fill(0);
+    }
+    return finalarr;
+}
+}
+
+
+
+
